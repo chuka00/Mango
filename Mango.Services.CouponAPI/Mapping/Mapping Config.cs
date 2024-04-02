e315a1fdd417d973040f6cede2aa9fd7e0c6d073
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-04-02 15:03:21 +0100

    Added Mapping class

---
+++ Mango.Services.CouponAPI/Mapping/Mapping Config.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
+
+namespace Mango.Services.CouponAPI.Mapping
+{
+    public class Mapping_Config
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
