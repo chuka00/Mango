b16767ec7879bd37be7c7593f0d3f335a55eba0a
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-04-03 13:34:57 +0100

    Added delete method

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -95,6 +95,40 @@ namespace Mango.Services.CouponAPI.Controllers
             }
             return _response;
         }
+        [HttpPut]
+        public ResponseDto UpdateCoupon([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _db.Coupons.Update(obj);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpDelete]
+        public ResponseDto DeleteCoupon(int id)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.First(u => u.CouponId == id);
+                _db.Coupons.Remove(coupon);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
 
     }
 }
