f2c5ad59c7f0fdc1f0b4281730266334e4d6e4bb
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-04-05 19:24:18 +0100

    Added Base service

---
+++ Mango.Web/Service/BaseService.cs
@@ -0,0 +1,27 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class BaseService : IBaseService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+
+        public BaseService(IHttpClientFactory httpClientFactory)
+        {
+
+            _httpClientFactory = httpClientFactory;
+
+        }
+        public async Task<ResponseDto> SendAsync(RequestDto requestDto)
+        {
+            HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+            HttpRequestMessage message = new();
+            message.Headers.Add("Accept", "application/json");
+            //Token
+
+            message.RequestUri = new Uri(requestDto.Url);
+        }
+    }
+}
