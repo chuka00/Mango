91f8dc858504c8b2f40866e6070ec52d543efc12
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-04-16 21:38:56 +0100

    Added Delete method

--- Mango.Web/Controllers/CouponController.cs
+++ Mango.Web/Controllers/CouponController.cs
@@ -44,6 +44,16 @@ namespace Mango.Web.Controllers
             return View(model);
         }
 
+        public async Task<IActionResult> CouponDelete(int couponId)
+        {
+            ResponseDto response = await _couponService.GetCouponByIdAsync(couponId);
 
+            if (response != null && response.IsSuccess)
+            {
+                CouponDto model = JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
     }
 }
