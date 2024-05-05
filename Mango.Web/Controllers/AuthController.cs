d8a83565bcccb1addd6cff2220a2fb3743f9c495
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-05-05 21:53:47 +0100

    Injected AuthService

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Web.Controllers
 {
     public class AuthController : Controller
     {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
         public IActionResult Index()
         {
             return View();
