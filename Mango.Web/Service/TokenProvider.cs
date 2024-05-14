78304f5939818adb4d54b1aed98b817db64f1ccd
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-05-14 22:49:34 +0100

    Added Token Provider class that inherits from the interface to handle tokens

---
+++ Mango.Web/Service/TokenProvider.cs
@@ -0,0 +1,22 @@
+﻿using Mango.Web.Service.IService;
+
+namespace Mango.Web.Service
+{
+    public class TokenProvider : ITokenProvider
+    {
+        public void ClearToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? GetToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetToken(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
