7af6476b92272876587e8fdc720f98b416f211cc
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-05-15 23:43:00 +0100

    unexpected bugs fixed

--- Mango.Web/Service/CouponService.cs
+++ Mango.Web/Service/CouponService.cs
@@ -19,7 +19,7 @@ namespace Mango.Web.Service
             {
                 ApiType = SD.ApiType.POST,
                 Data=couponDto,
-                Url = SD.CouponApiBase + "/api/coupon/"
+                Url = SD.CouponAPIBase + "/api/coupon/"
             });
         }
 
@@ -28,7 +28,7 @@ namespace Mango.Web.Service
             return await _baseService.SendAsync(new RequestDto
             {
                 ApiType = SD.ApiType.DELETE,
-                Url=SD.CouponApiBase+"/api/coupon/" + id
+                Url=SD.CouponAPIBase +"/api/coupon/" + id
             });
         }
 
@@ -37,7 +37,7 @@ namespace Mango.Web.Service
             return await _baseService.SendAsync(new RequestDto
             {
                 ApiType=SD.ApiType.GET,
-                Url=SD.CouponApiBase+"/api/coupon"
+                Url=SD.CouponAPIBase+"/api/coupon"
             });
         }
 
@@ -46,7 +46,7 @@ namespace Mango.Web.Service
             return await _baseService.SendAsync(new RequestDto
             {
                 ApiType = SD.ApiType.GET,
-                Url = SD.CouponApiBase + "/api/coupon/GetByCode/"+couponCode
+                Url = SD.CouponAPIBase + "/api/coupon/GetByCode/"+couponCode
             });
         }
 
@@ -55,7 +55,7 @@ namespace Mango.Web.Service
             return await _baseService.SendAsync(new RequestDto
             {
                 ApiType = SD.ApiType.GET,
-                Url = SD.CouponApiBase + "/api/coupon/GetByCode/" + id
+                Url = SD.CouponAPIBase + "/api/coupon/GetByCode/" + id
             });
         }
 
@@ -65,7 +65,7 @@ namespace Mango.Web.Service
             {
                 ApiType = SD.ApiType.PUT,
                 Data = couponDto,
-                Url = SD.CouponApiBase + "/api/coupon/"
+                Url = SD.CouponAPIBase + "/api/coupon/"
             });
         }
     }
