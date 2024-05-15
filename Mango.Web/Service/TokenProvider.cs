2c1c5f7a40433151f8ad15aa1153e79526043942
Chuka Ogbodo <chukaogbodo7@gmail.com>
2024-05-15 23:14:25 +0100

    bugs fixes

--- Mango.Web/Service/TokenProvider.cs
+++ Mango.Web/Service/TokenProvider.cs
@@ -1,22 +1,32 @@
 ﻿using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 
 namespace Mango.Web.Service
 {
     public class TokenProvider : ITokenProvider
     {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public TokenProvider(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
         public void ClearToken()
         {
-            throw new NotImplementedException();
+            _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.TokenCookie);
         }
 
         public string? GetToken()
         {
-            throw new NotImplementedException();
+            string? token = null;
+            bool? hasToken = _contextAccessor.HttpContext?.Request.Cookies.TryGetValue(SD.TokenCookie, out token);
+            return hasToken is true ? token : null;
         }
 
         public void SetToken(string token)
         {
-            throw new NotImplementedException();
+            _contextAccessor.HttpContext?.Response.Cookies.Append(SD.TokenCookie, token);
         }
     }
 }
